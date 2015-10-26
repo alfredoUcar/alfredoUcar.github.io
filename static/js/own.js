a13e5db7d21894938165e202a207c2032ee3007d
alfredo <alfredo.ucar@gmail.com>
2015-10-26 12:50:25 +0100

    introducción de canvas como capa para pintar sobre la imagen de fondo

--- static/js/own.js
+++ static/js/own.js
@@ -1,6 +1,15 @@
+//coordinates (x,y) of image reference for each section.
+//Coordinates are in % relative to the image
+var sectionsImageReference = {
+                                Section2:{10,15},
+                                Section3:{70,20}
+                                }
+
+
 
 $(document).ready(function() {
    dinamicResponsive();
+   canvasLayers();
 });
 
 $(window).resize(function() {
@@ -50,4 +59,27 @@ function checkSectionsVisibility(){
         }
         $($section).css( "opacity", opacity );
     }
+}
+
+function canvasLayers(){
+    //$("section").children("canvas").css();
+
+    $sections = $("section");
+    for (i = 0; i < $sections.length; ++i) {
+        $section = $($sections[i]);
+        var canvas = $section.find("canvas");
+        if (canvas.length == 0) continue;
+        var w=canvas.innerWidth,h=innerHeight;
+        var ctx = canvas.getContext("2d");
+        var id = $section.attr("id");
+        var pos = sectionsImageReference[id];
+        var x=pos[0]*w;
+        var y=pos[1]*h;
+        ctx.beginPath();
+        ctx.arc(x, y, 15, 0, 2 * Math.PI);
+        ctx.lineWidth=2;
+        ctx.strokeStyle="blue";
+        ctx.stroke();
+    }
+
 }
